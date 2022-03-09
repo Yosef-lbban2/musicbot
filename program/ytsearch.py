a6a2731c67be5ea253b5e750934d4b83411fe64e
alkisi78a <100542699+alkisi78a@users.noreply.github.com>
2022-03-09 16:05:06 +0300

    Update ytsearch.py

--- program/ytsearch.py
+++ program/ytsearch.py
@@ -16,14 +16,14 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@Client.on_message(command(["search", f"search@{BOT_USERNAME}"]))
+@Client.on_message(command(["روابط", f"search@{BOT_USERNAME}"]))
 async def ytsearch(_, message: Message):
 
     keyboard = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    "🗑 Close", callback_data="cls",
+                    "🗑 اغلاق", callback_data="cls",
                 )
             ]
         ]
@@ -34,15 +34,15 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search **needs an argument !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **Searching...**")
+        m = await message.reply_text("🔎 **يبحث...**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"🏷 **Name:** __{results[i]['title']}__\n"
-            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
-            text += f"👀 **Views:** `{results[i]['views']}`\n"
-            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🏷 **العنوان:** __{results[i]['title']}__\n"
+            text += f"⏱ **مدة:** `{results[i]['duration']}`\n"
+            text += f"👀 **المشاهدات:** `{results[i]['views']}`\n"
+            text += f"📣 **القناة:** {results[i]['channel']}\n"
             text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, reply_markup=keyboard, disable_web_page_preview=True)
