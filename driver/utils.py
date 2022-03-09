849ffc9a1ea6f41789aadcde49209f67862d150d
alkisi78a <100542699+alkisi78a@users.noreply.github.com>
2022-03-09 17:00:10 +0300

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -111,11 +111,11 @@ async def stream_end_handler(_, u: Update):
         print(chat_id)
         op = await skip_current_song(chat_id)
         if op==1:
-           await bot.send_message(chat_id, "✅ **userbot has disconnected from video chat.**")
+           await bot.send_message(chat_id, "✅ **تم قطع اتصال البوت من دردشة الفيديو.**")
         elif op==2:
-           await bot.send_message(chat_id, "❌ **an error occurred**\n\n» **Clearing** __Queues__ **and leaving video chat.**")
+           await bot.send_message(chat_id, "❌ **حدث خطأ**\n\n» **المقاسه** __قوائم الانتظار__ **ومغادره دردشة  الفيديو.**")
         else:
-         await bot.send_message(chat_id, f"💡 **Streaming next track**\n\n🏷 **Name:** [{op[0]}]({op[1]}) | `{op[2]}`\n💭 **Chat:** `{chat_id}`", disable_web_page_preview=True, reply_markup=keyboard)
+         await bot.send_message(chat_id, f"💡 **يجري المسار التالي**\n\n🏷 **العنوان:** [{op[0]}]({op[1]}) | `{op[2]}`\n💭 **الدردشة:** `{chat_id}`", disable_web_page_preview=True, reply_markup=keyboard)
     else:
        pass
 
