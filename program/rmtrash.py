de27532d2e2723b1d3bb8adc795a460ef39bbe47
X36X <97021126+X36X@users.noreply.github.com>
2022-03-15 18:03:59 +0300

    Update rmtrash.py

--- program/rmtrash.py
+++ program/rmtrash.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2021 By VeezMusicProject
-
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -9,7 +7,7 @@ from driver.decorators import sudo_users_only, errors
 downloads = os.path.realpath("program/downloads")
 raw = os.path.realpath(".")
 
-@Client.on_message(command(["rmd", "clear"]) & ~filters.edited)
+@Client.on_message(command(["rmd", "تنظيف التنزيلات"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_downloads(_, message: Message):
@@ -17,12 +15,12 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("✅ **deleted all downloaded files**")
+        await message.reply_text("✅ **حذف جميع التنزيلات**")
     else:
-        await message.reply_text("❌ **no files downloaded**")
+        await message.reply_text("❌ **لايوجد ملفات منزله**")
 
         
-@Client.on_message(command(["rmw", "clean"]) & ~filters.edited)
+@Client.on_message(command(["rmw", "حذف الملفات"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_raw(_, message: Message):
@@ -31,12 +29,12 @@ async def clear_raw(_, message: Message):
         for file in os.listdir(raw):
             if file.endswith('.raw'):
                 os.remove(os.path.join(raw, file))
-        await message.reply_text("✅ **deleted all raw files**")
+        await message.reply_text("✅ **حذف جميع الملفات**")
     else:
-        await message.reply_text("❌ **no raw files found**")
+        await message.reply_text("❌ **لايوجد ملفات**")
 
 
-@Client.on_message(command(["cleanup"]) & ~filters.edited)
+@Client.on_message(command(["تنظيف"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def cleanup(_, message: Message):
@@ -45,6 +43,6 @@ async def cleanup(_, message: Message):
     if ls_dir:
         for dta in os.listdir(pth):
             os.system("rm -rf *.raw *.jpg")
-        await message.reply_text("✅ **cleaned**")
+        await message.reply_text("✅ **تم تنضيف**")
     else:
-        await message.reply_text("✅ **already cleaned**")
+        await message.reply_text("✅ **بالفعل تم تنضيف**")
