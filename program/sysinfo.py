1005cc150298bc3ec9599c68df80c8e422f93093
X36X <97021126+X36X@users.noreply.github.com>
2022-03-15 18:00:56 +0300

    Update sysinfo.py

--- program/sysinfo.py
+++ program/sysinfo.py
@@ -14,7 +14,7 @@ from driver.decorators import sudo_users_only, humanbytes
 
 # FETCH SYSINFO
 
-@Client.on_message(command(["sysinfo", f"sysinfo@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["المعلومات", f"المعلومات@{BOT_USERNAME}"]) & ~filters.edited)
 @sudo_users_only
 async def give_sysinfo(client, message):
     splatform = platform.system()
@@ -35,19 +35,9 @@ async def give_sysinfo(client, message):
     psutil.disk_io_counters()
     disk = f"{humanbytes(du.used)} / {humanbytes(du.total)} " f"({du.percent}%)"
     cpu_len = len(psutil.Process().cpu_affinity())
-    somsg = f"""🖥 **System Information**
+    somsg = f"""🖥 **السستم**
     
-**PlatForm :** `{splatform}`
-**PlatForm - Release :** `{platform_release}`
-**PlatFork - Version :** `{platform_version}`
-**Architecture :** `{architecture}`
-**Hostname :** `{hostname}`
-**IP :** `{ip_address}`
-**Mac :** `{mac_address}`
-**Processor :** `{processor}`
-**Ram : ** `{ram}`
-**CPU :** `{cpu_len}`
-**CPU FREQ :** `{cpu_freq}`
-**DISK :** `{disk}`
-    """
+**برنامج :** `{splatform}`
+**برنامج - إطلاق :** `{platform_release}`
+**الإصدار :** `1.0`"""
     await message.reply(somsg)
